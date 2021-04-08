d257e15073a6f16180c0926e1fe86328cdb9caa3
kim_haaga <kim.haaga@live.se>
2021-04-08 20:20:19 +0200

    Add About and Projects .jsx files, update App.jsx with import About, Projects and react-router-dom update Header.jsx with react-router-dom and added the tabs to the nav

---
+++ src/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from "semantic-ui-react";
+
+const About = () => {
+  return (
+    <Container>
+      <h1 id="about-header">About Me</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero eligendi deserunt, commodi ullam unde a at cumque pariatur debitis quos qui? Corporis officia voluptatem sint nam? A perferendis explicabo quis.
+      </p>
+    </Container>
+  );
+};
+
+export default About
---
+++ src/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from "semantic-ui-react";
+
+const Projects = () => {
+  return (
+   <Container>
+     <h1 id="projects-header">My Projects</h1>
+     <p>
+       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis vitae mollitia numquam nulla, ipsa dolor non consequuntur unde et aspernatur assumenda quia eos illo repellat recusandae sit cum? Quaerat, odit.
+     </p>
+   </Container>
+  )
+}
+
+export default Projects;
