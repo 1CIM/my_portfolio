a28b99fb47589d230838863b971c61020b5ff01a
kim_haaga <kim.haaga@live.se>
2021-04-09 12:01:14 +0200

    Revert back to working commit before adding unDraw

--- src/About.jsx
+++ src/About.jsx
@@ -1,23 +1,13 @@
-import React from "react";
-import { Container, Grid } from "semantic-ui-react";
-import { UndrawDesignerLife } from "react-undraw-illustrations";
+import React from 'react';
+import { Container } from "semantic-ui-react";
 
 const About = () => {
   return (
     <Container>
-      <Grid>
-        <Grid.Row columns={2}>
-          <Grid.Column>
-            <UndrawDesignerLife primaryColor='#12283a' height='200px' />
-          </Grid.Column>
-          <Grid.Column>
-            <h1 id="about-header">About Me</h1>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero eligendi deserunt, commodi ullam unde a at cumque pariatur debitis quos qui? Corporis officia voluptatem sint nam? A perferendis explicabo quis.
-            </p>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+      <h1 id="about-header">About Me</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero eligendi deserunt, commodi ullam unde a at cumque pariatur debitis quos qui? Corporis officia voluptatem sint nam? A perferendis explicabo quis.
+      </p>
     </Container>
   );
 };
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Container, Grid } from "semantic-ui-react";
-import { UndrawDashboard } from "react-undraw-illustrations";
 import ProjectCard from './ProjectCard';
 import axios from "axios";
 
@@ -22,20 +21,7 @@ class Projects extends Component {
 
     return (
       <Container>
-        <Grid>
-          <Grid.Row columns={2}>
-            <Grid.Column>
-              <UndrawDashboard primaryColor='#12283a' height='200px' />
-            </Grid.Column>
-            <Grid.Column>
-            <h1 id="projects-header">My Projects</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, atque eum eaque ut perspiciatis quo consectetur quis, at impedit est libero corrupti ea voluptate cum minima expedita qui nostrum quod?
-            </p>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-        
+        <h1 id="projects-header">My Projects</h1>
         <Grid>{projectsList}</Grid>
       </Container>
     );
