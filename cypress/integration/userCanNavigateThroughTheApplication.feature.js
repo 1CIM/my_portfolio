77c865c4ca1a9fa75f455cf0b36b5b4f0fbf0e3e
kim_haaga <kim.haaga@live.se>
2021-04-10 00:11:17 +0200

    Implement background test to navigate test

--- cypress/integration/userCanNavigateThroughTheApplication.feature.js
+++ cypress/integration/userCanNavigateThroughTheApplication.feature.js
@@ -12,6 +12,10 @@ describe("User can navigate to the app", () => {
       cy.get("[data-cy=about-header]").should("contain", "About Me");
     });
 
+    it("displays about background image", () => {
+      cy.get("[data-cy=bg-container-2]").should("exist");
+    });
+
     it("displays components name in URL", () => {
       cy.url().should("contain", "about");
     });
@@ -20,14 +24,22 @@ describe("User can navigate to the app", () => {
       cy.get("[data-cy=projects-header]").should("not.exist");
     });
 
-    it("dose not display Hello World", () => {
-      cy.get("[data-cy=bg-container]").should("not.exist");
+    it("dose not display home page background image", () => {
+      cy.get("[data-cy=bg-container-1]").should("not.exist");
+    });
+
+    it("dose not display projects background image", () => {
+      cy.get("[data-cy=bg-container-3]").should("not.exist");
     });
   });
   
   describe("to My Projects tab and it", () => {
     beforeEach(() => {
       cy.get("[data-cy=projects-tab]").click();
+    });
+
+    it("displays about background image", () => {
+      cy.get("[data-cy=bg-container-3]").should("exist");
     });
 
     it("displays My Projects header", () => {
@@ -42,19 +54,23 @@ describe("User can navigate to the app", () => {
       cy.get("[data-cy=about-header]").should("not.exist");
     });
 
-    it("dose not display Hello World", () => {
-      cy.get("[data-cy=bg-container]").should("not.exist");
+    it("dose not display home page background image", () => {
+      cy.get("[data-cy=bg-container-1]").should("not.exist");
+    });
+
+    it("dose not display projects background image", () => {
+      cy.get("[data-cy=bg-container-2]").should("not.exist");
     });
   });
 
   describe("back to My Portfolio/Hello tab and it", () => {
     beforeEach(() => {
       cy.get("[data-cy=about-tab]").click();
-      cy.get("[data-cy=bg-container]").click();
+      cy.get("[data-cy=header]").click();
     });
 
     it("displays background image", () => {
-      cy.get("[data-cy=bg-container]").should("exist");
+      cy.get("[data-cy=bg-container-1]").should("exist");
     });
 
     it("displays correct URL", () => {
@@ -66,9 +82,17 @@ describe("User can navigate to the app", () => {
     it("dose not display About Me header", () => {
       cy.get("[data-cy=about-header]").should("not.exist");
     });
-
+    
     it("dose not display My Projects header", () => {
       cy.get("[data-cy=projects-header]").should("not.exist");
+    });
+
+    it("dose not display home page background image", () => {
+      cy.get("[data-cy=bg-container-2]").should("not.exist");
+    });
+
+    it("dose not display projects background image", () => {
+      cy.get("[data-cy=bg-container-3]").should("not.exist");
     });
   });
 });
