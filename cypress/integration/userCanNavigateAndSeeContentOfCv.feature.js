0ec46fe60b3f3df12dcf9fca75d004d611c42f24
kim_haaga <kim.haaga@live.se>
2021-04-10 23:12:37 +0200

    Extend test for cv

--- cypress/integration/userCanNavigateAndSeeContentOfCv.feature.js
+++ cypress/integration/userCanNavigateAndSeeContentOfCv.feature.js
@@ -3,8 +3,31 @@ describe("User can navigate to cv and see content", () => {
     cy.visit("/");
     cy.get("[data-cy=cv-tab]").click();
   });
+  
+  it("displays components name in URL", () => {
+    cy.url().should("contain", "CV")
+  });
 
   it("displays cv header", () => {
     cy.get("[data-cy=cv-tab]").should("contain", "CV");
   });
+
+  it("displays job experiences", () => {
+    cy.get("[data-cy=job-xp]")
+    .should("contain", "Thin Smith, Bartender, Waiter");
+  });
+
+  it("displays previous education", () => {
+    cy.get("[data-cy=prev-educ]")
+    .should("contain", "Electrical automation, Bartender & Waiter, Sommelier step 1, Korean");
+  });
+
+  it("displays background image", () => {
+    cy.get("[data-cy=cv-bg]").should("exist");
+  });
+
+  it("dose not display my portfolio background image", () => {
+    cy.get("[data-cy=home-bg]").should("not.exist");
+  });
+
 });
