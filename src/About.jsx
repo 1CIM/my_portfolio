d867d529e23c49678afb4916188cb335d5fc795f
kim_haaga <kim.haaga@live.se>
2021-04-10 22:14:27 +0200

    Add style to About me

--- src/About.jsx
+++ src/About.jsx
@@ -5,10 +5,12 @@ const About = () => {
   return (
     <Container class='bg-container' fluid>
       <Image data-cy="about-bg" id='background' src='https://images.unsplash.com/photo-1526199119161-4be1e3368d52?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80' fluid centered/>
+      <Container textAlign='center'>
       <h1 data-cy="about-header" id="about-header">About Me</h1>
-      <p>
+      <p class="about-p">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero eligendi deserunt, commodi ullam unde a at cumque pariatur debitis quos qui? Corporis officia voluptatem sint nam? A perferendis explicabo quis.
       </p>
+      </Container>
     </Container>
   );
 };
