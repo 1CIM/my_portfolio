a74c9f0a7daf7821cf3fdcd63c3e4a739c492f4e
kim_haaga <kim.haaga@live.se>
2021-04-10 21:59:54 +0200

    Fixed project style

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -22,8 +22,10 @@ class Projects extends Component {
     return (
       <Container class="bg-container" fluid>
         <Image data-cy="projects-bg" id="background" src="https://images.unsplash.com/photo-1598349326101-fc9e68975a52?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" fluid centered />
+        <Container textAlign='center' >
         <h1 data-cy="projects-header" id="projects-header">My Projects</h1>
-        <Grid>{projectsList}</Grid>
+        <Grid class="center" centered columns={3} >{projectsList}</Grid>  
+        </Container>
       </Container>
     );
   }
