86506a98378e8ab9251639cb3b32b0a667f6492b
kim_haaga <kim.haaga@live.se>
2021-04-11 22:57:02 +0200

    Add contact background image

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Form, Message, Segment } from "semantic-ui-react";
+import { Container, Form, Message, Segment, Image } from "semantic-ui-react";
 
 const encode = (data) => {
   return Object.keys(data)
@@ -31,9 +31,10 @@ class ContactForm extends Component {
     const { name, email, message } = this.state;
     return (
       <Container class="bg-container" fluid>
+        <Image data-cy="contact-bg" id="background" src="https://images.unsplash.com/photo-1535350356005-fd52b3b524fb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" fluid centered/>
         <Container textAlign="center">
           <Segment inverted>
-            <Form inverted onSubmit={this.handleSubmit} netlify name="contact" data-cy="contact-form">
+            <Form onSubmit={this.handleSubmit} netlify name="contact" data-cy="contact-form">
               <Form.Input type="hidden" name="form-name" value="contact" />
               <p>
                 <label>
